7f9e3614f55b3151083e3d64974a172e36a539ea
Newty19 <a334144@uach.mx>
2023-11-15 01:35:26 -0600

    Cors working

--- app.js
+++ app.js
@@ -26,16 +26,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors({
   origin:[
-    "*"
+    "*",
+    "localhost:8080",
   ]
 }));
-
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/customers',customersRouter);
 app.use('/order_items',orderitemsRouter);
 app.use('/orders',ordersRouter);
 app.use('/products',productsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
